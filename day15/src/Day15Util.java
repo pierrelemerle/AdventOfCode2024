653b4b705a0f747cce84682b0702e24d06015422
Pierre Lemerle <pierre.lemerle@gmail.com>
2024-12-18 22:32:27 +0100

    day 15, fixed the corner case left for part2

--- day15/src/Day15Util.java
+++ day15/src/Day15Util.java
@@ -1,10 +1,11 @@
 import java.awt.Point;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Day15Util {
 
     // to simulate movement of the robot for the whole instruction list.
-    public static void moveRobots(char[][] map, List<Character> instructionList) {
+    public static void moveRobotsPart1(char[][] map, List<Character> instructionList) {
         for (int i = 0; i < instructionList.size(); i++) {
             Point robotPosition = new Point();
             for (int y = 0; y < map.length; y++) {
@@ -20,7 +21,7 @@ public class Day15Util {
     }
 
     // to calculate the result for part1
-    public static Long coordinateCalulation(char[][] map) {
+    public static Long coordinateCalulationPart1(char[][] map) {
         Long total = 0L;
         for (int y = 0; y < map.length; y++) {
             for (int x = 0; x < map[y].length; x++) {
@@ -99,4 +100,277 @@ public class Day15Util {
             map[nextY][nextX] = 'O';
         }
     }
+
+    // transform the part1 input map to the needed one for part2
+    public static char[][] transformMapToPart2(char[][] mapPart1) {
+        char[][] mapPart2 = new char[mapPart1.length][mapPart1[0].length * 2];
+
+        for (int y = 0; y < mapPart1.length; y++) {
+            for (int x = 0; x < mapPart1[y].length; x++) {
+                if (mapPart1[y][x] == '#') {
+                    mapPart2[y][2 * x] = '#';
+                    mapPart2[y][2 * x + 1] = '#';
+                }
+                if (mapPart1[y][x] == '.') {
+                    mapPart2[y][2 * x] = '.';
+                    mapPart2[y][2 * x + 1] = '.';
+                }
+                if (mapPart1[y][x] == 'O') {
+                    mapPart2[y][2 * x] = '[';
+                    mapPart2[y][2 * x + 1] = ']';
+                }
+                if (mapPart1[y][x] == '@') {
+                    mapPart2[y][2 * x] = '@';
+                    mapPart2[y][2 * x + 1] = '.';
+                }
+            }
+        }
+        return mapPart2;
+    }
+
+    // to simulate movement of the robot for the whole instruction list.
+    public static void moveRobotsPart2(char[][] map, List<Character> instructionList) {
+        for (int i = 0; i < instructionList.size(); i++) {
+            Point robotPosition = new Point();
+            for (int y = 0; y < map.length; y++) {
+                for (int x = 0; x < map[y].length; x++) {
+                    if (map[y][x] == '@') {
+                        robotPosition.x = x;
+                        robotPosition.y = y;
+                    }
+                }
+            }
+            if (instructionList.get(i) == '^') {
+                moveUp(map, robotPosition);
+            } else if (instructionList.get(i) == '>') {
+                moveRight(map, robotPosition);
+            } else if (instructionList.get(i) == 'v') {
+                moveDown(map, robotPosition);
+            } else if (instructionList.get(i) == '<') {
+                moveLeft(map, robotPosition);
+            }
+        }
+    }
+
+    // move the robot for a single move right for the part2 map
+    private static void moveRight(char[][] map, Point robotPosition) {
+
+        int y = robotPosition.y;
+        int x = robotPosition.x;
+        int nextY = y;
+        int nextX = x + 1;
+
+        if (map[nextY][nextX] == '.') {
+            map[y][x] = '.';
+            map[nextY][nextX] = '@';
+            return;
+        } else if (map[nextY][nextX] == '#') {
+            // nothing to do
+            return;
+        }
+        // other wise then it is a box need to verify if we will move, or Not at all
+        while (map[nextY][nextX] != '.') {
+            nextX++;
+            // if we can not do this move then we quit
+            if ((nextX >= map[nextY].length) || (map[nextY][nextX] == '#'))
+                return;
+        }
+        map[nextY][x] = '.';
+        map[nextY][x + 1] = '@';
+        // update the last box
+        map[nextY][nextX] = ']';
+        // need to switch all the boxes and change all the char from y x+2 until y nextX
+        for (int j = x + 2; j < nextX; j++) {
+            if (map[nextY][j] == '[')
+                map[nextY][j] = ']';
+            else
+                map[nextY][j] = '[';
+        }
+    }
+
+    // move the robot for a single move Left for the part2 map
+    private static void moveLeft(char[][] map, Point robotPosition) {
+
+        int y = robotPosition.y;
+        int x = robotPosition.x;
+        int nextY = y;
+        int nextX = x - 1;
+
+        if (map[nextY][nextX] == '.') {
+            map[y][x] = '.';
+            map[nextY][nextX] = '@';
+            return;
+        } else if (map[nextY][nextX] == '#') {
+            // nothing to do
+            return;
+        }
+        // other wise then it is a box need to verify if we will move, or Not at all
+        while (map[nextY][nextX] != '.') {
+            nextX--;
+            // we can not do this move
+            if ((nextX == 0) || (map[nextY][nextX] == '#'))
+                return;
+        }
+        map[nextY][x] = '.';
+        map[nextY][x - 1] = '@';
+        // update the last box
+        map[nextY][nextX] = '[';
+        // need to switch all the boxes and change all the char from y x+2 until y nextX
+        for (int j = x - 2; j > nextX; j--) {
+            if (map[nextY][j] == '[')
+                map[nextY][j] = ']';
+            else
+                map[nextY][j] = '[';
+        }
+    }
+
+    // move the robot for a single move for the part2 map
+    private static void moveUp(char[][] map, Point robotPosition) {
+
+        int y = robotPosition.y;
+        int x = robotPosition.x;
+        int nextY = y - 1;
+        int nextX = x;
+
+        if (map[nextY][nextX] == '.') {
+            map[y][x] = '.';
+            map[nextY][nextX] = '@';
+            return;
+        } else if (map[nextY][nextX] == '#') {
+            // nothing to do
+            return;
+        }
+
+        List<Point> pointToMove = new ArrayList<>();
+        pointToMove.add(new Point(x, y));
+        AtomicBoolean movePossible = new AtomicBoolean(true);
+
+        moveUpRecursive(map, pointToMove, movePossible, y, x);
+        if (!movePossible.get())
+            return;
+
+        // we need to update all the point to move in the list.
+        // filter the duplicate to avoid issues
+        pointToMove = getUniquePoints(pointToMove);
+        // Sort the points
+        pointToMove.sort((p1, p2) -> {
+            if (p1.y != p2.y) {
+                return Integer.compare(p1.y, p2.y); // Compare by y-coordinate
+            }
+            return Integer.compare(p1.x, p2.x); // Compare by x-coordinate if y is the same
+        });
+
+        for (int i = 0; i < pointToMove.size(); i++) {
+            y = pointToMove.get(i).y;
+            x = pointToMove.get(i).x;
+            map[y - 1][x] = map[y][x];
+            map[y][x] = '.';
+        }
+
+    }
+
+    // move the robot for a single move for the part2 map
+    private static void moveDown(char[][] map, Point robotPosition) {
+
+        int y = robotPosition.y;
+        int x = robotPosition.x;
+        int nextY = y + 1;
+        int nextX = x;
+
+        if (map[nextY][nextX] == '.') {
+            map[y][x] = '.';
+            map[nextY][nextX] = '@';
+            return;
+        } else if (map[nextY][nextX] == '#') {
+            // nothing to do
+            return;
+        }
+
+        List<Point> pointToMove = new ArrayList<>();
+        pointToMove.add(new Point(x, y));
+        AtomicBoolean movePossible = new AtomicBoolean(true);
+
+        moveDownRecursive(map, pointToMove, movePossible, y, x);
+        if (!movePossible.get())
+            return;
+
+        // we need to update all the point to move in the list.
+        // filter the duplicate to avoid issues
+        pointToMove = getUniquePoints(pointToMove);
+        // Sort the points in reverse order
+        pointToMove.sort((p1, p2) -> {
+            if (p1.y != p2.y) {
+                return Integer.compare(p2.y, p1.y); // Compare by y-coordinate in descending order
+            }
+            return Integer.compare(p2.x, p1.x); // Compare by x-coordinate in descending order if y is the same
+        });
+
+        for (int i = 0; i < pointToMove.size(); i++) {
+            y = pointToMove.get(i).y;
+            x = pointToMove.get(i).x;
+            map[y + 1][x] = map[y][x];
+            map[y][x] = '.';
+        }
+
+    }
+
+    private static void moveUpRecursive(char[][] map, List<Point> pointToMove, AtomicBoolean movePossible, int y,
+            int x) {
+        if (map[y - 1][x] == '#')
+            movePossible.set(false);
+        if (map[y - 1][x] == '[') {
+            pointToMove.add(new Point(x, y - 1));
+            pointToMove.add(new Point(x + 1, y - 1));
+            moveUpRecursive(map, pointToMove, movePossible, y - 1, x);
+            moveUpRecursive(map, pointToMove, movePossible, y - 1, x + 1);
+        }
+        if (map[y - 1][x] == ']') {
+            pointToMove.add(new Point(x - 1, y - 1));
+            pointToMove.add(new Point(x, y - 1));
+            moveUpRecursive(map, pointToMove, movePossible, y - 1, x - 1);
+            moveUpRecursive(map, pointToMove, movePossible, y - 1, x);
+        }
+    }
+
+    private static void moveDownRecursive(char[][] map, List<Point> pointToMove, AtomicBoolean movePossible, int y,
+            int x) {
+        if (map[y + 1][x] == '#')
+            movePossible.set(false);
+        ;
+        if (map[y + 1][x] == '[') {
+            pointToMove.add(new Point(x, y + 1));
+            pointToMove.add(new Point(x + 1, y + 1));
+            moveDownRecursive(map, pointToMove, movePossible, y + 1, x);
+            moveDownRecursive(map, pointToMove, movePossible, y + 1, x + 1);
+        }
+        if (map[y + 1][x] == ']') {
+            pointToMove.add(new Point(x - 1, y + 1));
+            pointToMove.add(new Point(x, y + 1));
+            moveDownRecursive(map, pointToMove, movePossible, y + 1, x - 1);
+            moveDownRecursive(map, pointToMove, movePossible, y + 1, x);
+        }
+    }
+
+    // to calculate the result for part2
+    public static Long coordinateCalulationPart2(char[][] map) {
+        Long total = 0L;
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[y].length; x++) {
+                if (map[y][x] == '[') {
+                    Long thisBox = 100L * y + x;
+                    total += thisBox;
+                }
+            }
+        }
+        return total;
+    }
+
+    // filter the points via a SET to avoid issues in the points update
+    private static List<Point> getUniquePoints(List<Point> points) {
+        // Use a HashSet to filter out duplicates
+        Set<Point> uniquePointsSet = new HashSet<>(points);
+        // Convert back to a list
+        return new ArrayList<>(uniquePointsSet);
+    }
+
 }
--- day15/src/Main.java
+++ day15/src/Main.java
@@ -9,26 +9,33 @@ public class Main {
 
         // initialise an empty grid of the right size
         List<Character> movements = new ArrayList<>();
-        char[][] map = Helper.readFileToTabTabCharAndInstruction(filePath, movements);
+        char[][] mapPart1 = Helper.readFileToTabTabCharAndInstruction(filePath, movements);
+        char[][] mapPart2 = Day15Util.transformMapToPart2(mapPart1);
 
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[y].length; x++) {
-                System.out.print(map[y][x]);
-            }
-            System.out.println();
-        }
-        System.out.println(movements);
-        Day15Util.moveRobots(map, movements);
-
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[y].length; x++) {
-                System.out.print(map[y][x]);
-            }
-            System.out.println();
-        }
-        Long sumAllBoxes = Day15Util.coordinateCalulation(map);
         // for the part 1
+        Day15Util.moveRobotsPart1(mapPart1, movements);
+        Long sumAllBoxes = Day15Util.coordinateCalulationPart1(mapPart1);
         System.out.println("La somme des coordonees de toutes les boites pour la partie 1 est " + sumAllBoxes);
+
+        // for the part 2
+        for (int y = 0; y < mapPart2.length; y++) {
+            for (int x = 0; x < mapPart2[y].length; x++) {
+                System.out.print(mapPart2[y][x]);
+            }
+            System.out.println();
+        }
+
+        Day15Util.moveRobotsPart2(mapPart2, movements);
+
+        // for the part 2
+        for (int y = 0; y < mapPart2.length; y++) {
+            for (int x = 0; x < mapPart2[y].length; x++) {
+                System.out.print(mapPart2[y][x]);
+            }
+            System.out.println();
+        }
+        Long sumAllBoxes2 = Day15Util.coordinateCalulationPart2(mapPart2);
+        System.out.println("La somme des coordonees de toutes les boites pour la partie 2 est " + sumAllBoxes2);
 
     }
 
